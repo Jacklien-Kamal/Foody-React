4de5e4a56d3b1a6f479fa5906eeb36da0562c892
jacklien <jacklienkamal442@gmail.com>
2024-05-26 15:05:26 +0300

    AppStore

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar/Navbar'
 import Welcom from './components/welcome/welcome'
 import Services from './components/Services/Services'
 import Banner from './components/Banner/Banner'
+import AppStore from './components/AppStore/AppStore'
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
     <Welcom/>
     <Services/>
     <Banner/>
+    <AppStore/>
     </>
   )
 }
---
+++ src/components/AppStore/AppStore.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import bike from "../../assets/mobile_bike.gif";
+import appStore from "../../assets/app_store.png";
+import playStore from "../../assets/play_store.png";
+export default function () {
+  return (
+    <div className="bg-gray-100 ">
+      <div className="container py-12  grid grid-cols-1 sm:grid-cols-2 place-items-center ">
+        <div className="max-w-[450px]">
+          <p className="text-center text-gray-700 font-bold text-2xl  lg:text-3xl md:text-2xl sm:text-2xl  sm:text-left">
+            Foodly is available for Android and IOS{" "}
+          </p>
+          <div className="flex flex-wrap justify-center sm:justify-start items-center py-3">
+            <img
+              className="max-w-[150px] sm:max-w-[120px] md:max-w-[200px]"
+              src={playStore}
+            />
+            <img
+              className="max-w-[150px] sm:max-w-[120px] md:max-w-[200px]"
+              src={appStore}
+            />
+          </div>
+        </div>
+        <div>
+          <img
+            src={bike}
+            className="w-full sm:max-w-[60%] block rounded-md mx-auto mix-blend-multiply dark:mix-blend-difference"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
